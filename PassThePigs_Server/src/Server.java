2e92548624178c0cf0036a6657c8eebeaa454471
Anna Madej <aniamadej123@gmail.com>
2017-06-27 19:55:22 +0200

    poprawiłam wyświetlanie kounikatów i przekazywanie kolejki

--- PassThePigs_Server/src/Server.java
+++ PassThePigs_Server/src/Server.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.Vector;
 
@@ -44,8 +45,10 @@ class Uczestnik extends Thread {
     static Vector<Uczestnik> uczestnicy = new Vector<Uczestnik>(); // definicja zmiennej przechowującej wszystkich uczestnikow;
     static int polaczeniGracze = 0;  //definicja zmiennej przechowującej ilość podłączonych uczestników
     static Uczestnik aktualnyUczestnik;
-    int maxIloscGraczy;
-
+    static int maxIloscGraczy;
+    static int ktoZacznie = 0;
+    boolean pierwszy = false;
+    int punkty = 0;
     private Socket socket; // deklaracja socketu dla połączenia z uczestnikiem
     private BufferedReader in; // deklaracja strumienia danych otrzymanych od uczestnika
     private PrintWriter out; // deklaracja strumienia danych wysyłanych do uczestnika
@@ -53,21 +56,54 @@ class Uczestnik extends Thread {
 
     public Uczestnik(Socket socket, int maxIloscGraczy) { // konstruktor obsługi nowego połączenia)
         this.socket = socket;
-        this.maxIloscGraczy = maxIloscGraczy;
+        if (this.maxIloscGraczy == 0)
+        {
+        this.maxIloscGraczy = maxIloscGraczy; }
     }
 
+    private void wyslijDoJednego(Uczestnik uczestnik, String text)
+    {
+        uczestnik.out.println(text);
+    }
 
-    private void wyslijDoWszystkich(String tekst) { // metoda wysyłająca dane do wszytkich obecnych
+    private synchronized void wyslijDoWszystkich(String tekst) { // metoda wysyłająca dane do wszytkich uczestników
         for (Uczestnik uczestnik : uczestnicy) { // dla uczestników z listy uczestników
-            synchronized(uczestnicy) { // synchronizowane działanie (może być robione tylko przez jeden wątek na raz)
-           //     if (uczestnik != this)  // jeżeli gra nie jest bieżącą grą
-                    uczestnik.out.println("<" + nick + ">" + tekst); //do wszystkich innych wysyłamy napisany tekst
-            }
+                    uczestnik.out.println(tekst); //do wszystkich  wysyłamy  tekst
         }
     }
 
+    private synchronized  void wyslijDoInnych(Uczestnik zWyjatkiem, String tekst) { // metoda wysyłająca dane do pozostałych uczetników
+        for (Uczestnik uczestnik : uczestnicy) { // dla uczestników z listy uczestników
+                if (uczestnik != zWyjatkiem)  // jeżeli uczestnik nie jest wykluczonym uczestnikiem
+                uczestnik.out.println(tekst); //do wszystkich innych wysyłamy  tekst
+
+        }
+    }
+
+    private void graj() {
+
+        out.println("\nzdobyłeś punkt!");
+        punkty++;
+        out.println("\nMasz punktów:" + punkty);
+        out.println("[enter] - rzucaj");
+        out.println("/p - punkty");
+        out.println("/r - rezygnuj\n");
+        wyslijDoInnych(this, " Zdobył punkt!");
+    }
+
+    private void pokazPunkty() {
+        out.println("\n*****************");
+        out.println("TABELA PUNKTÓW:");
+
+        for (Uczestnik uczestnik : uczestnicy) { // dla uczestników z listy uczestników
+
+            out.println(uczestnik.nick + ": " + uczestnik.punkty + "pkt" );
+        }
+        out.println("*****************");
+    }
+
     private void users() { // metoda wyświetlająca informacje na temat użytkowników
-        out.print("Witaj " + nick + ", aktualnie grają: ");
+        out.println("Witaj " + nick + ", aktualnie grają: ");
         for (Uczestnik uczestnik : uczestnicy) { // dla wszystkich uczestników z listy uczestników
             synchronized(uczestnicy) { // synchronizowane działanie
                 if (uczestnik != this) // jeżeli uczestnik nie jest bieżącym uczestnikiem
@@ -77,6 +113,57 @@ class Uczestnik extends Thread {
         out.println();
     }
 
+    private synchronized void dolaczDoGry(){
+
+            uczestnicy.add(this); // dodanie bieżącego uczestnika do listy uczestników
+
+        System.out.println("Do gry dołączył: " + nick);                         // to wyświetlamy na serwerze
+        System.out.println("Połączonych graczy: " + uczestnicy.size()+ "/" + maxIloscGraczy);      // to też
+        wyslijDoInnych(this, "Do gry dołączył: " + nick);                         // wysyłamy do wszystkich
+        wyslijDoJednego(this, "Dołączyłeś do gry!");
+    }
+
+    private synchronized void opuscGre(){
+        wyslijDoWszystkich("Grę opuścił: " + nick);                              // jeżeli wpisał /q to opuszcza grę
+        if (this == aktualnyUczestnik) {
+            zakonczTure();
+        }
+            uczestnicy.removeElement(this);
+            maxIloscGraczy--;
+        System.out.println("Grę opuścił: " + nick);
+        System.out.println("Połączonych graczy: " + uczestnicy.size()+ "/" + maxIloscGraczy);
+
+    }
+
+    private synchronized void zakonczTure() {
+
+            if ((uczestnicy.indexOf(this) + 1 == uczestnicy.size())) {
+                ustawAktualnego(0);
+            } else {
+                ustawAktualnego(uczestnicy.indexOf(this) + 1);
+            }
+    }
+
+    private int losujKtoZacznie()
+    {
+        int ktoZacznie = (int)(Math.random() * maxIloscGraczy);
+        return ktoZacznie;
+    }
+
+    private synchronized  void ustawAktualnego(int numerAktualnego) {
+        aktualnyUczestnik = uczestnicy.get(numerAktualnego) ;
+        wyslijDoJednego(uczestnicy.get(numerAktualnego), "TWOJA KOLEJ!");
+        wyslijDoJednego(uczestnicy.get(numerAktualnego), "[enter] - rzucaj");
+        wyslijDoJednego(uczestnicy.get(numerAktualnego),"/r - rezygnuj");
+
+
+        wyslijDoInnych(aktualnyUczestnik, "Teraz rzuca: " + aktualnyUczestnik.nick);
+        wyslijDoWszystkich("/p - punkty");
+        wyslijDoWszystkich("/q - wyjscie");
+    }
+
+
+
     public void run() {
         String linia; // deklaracja napisu wpisanego przez użytkownika
 
@@ -85,43 +172,53 @@ class Uczestnik extends Thread {
             out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true); // definicja strumienia wyjściowego
 
             if (uczestnicy.size() < maxIloscGraczy) {
-                synchronized (uczestnicy) { //synchronizowane działanie
-                    uczestnicy.add(this); // dodanie bieżącego uczestnika do listy uczestników
-                    if (uczestnicy.size() == 1) aktualnyUczestnik = this;
-                }
 
-                out.println("Połączony z serwerem. Komenda /end kończy połączenie.");   // to wysyłamy do klienta
+                out.println("Połączony z serwerem. Komenda /q kończy połączenie.");   // to wysyłamy do klienta
                 out.println("Podaj swój nick: ");                                       // to też
                 nick = in.readLine();                                                   // odbieramy od klienta nick
-                System.out.println("Do gry dołączył: " + nick);                         // to wyświetlamy na serwerze
-                System.out.println("Połączonych graczy: " + uczestnicy.size()+ "/" + maxIloscGraczy);      // to też
-                wyslijDoWszystkich("Pojawił się w grze");                         // wysyłamy do wszystkich
-                users();                                                                // wyświetlamy klientowi info o użytkownikach
+
+                dolaczDoGry();
+
+                if (uczestnicy.size() == 1)
+                {
+                  pierwszy = true;
+                }
+
+                if (uczestnicy.size() != maxIloscGraczy) {
+                    out.println("Czekaj na dołączenie wszystkich graczy!");
+                    while (uczestnicy.size() != maxIloscGraczy) {
+                    }
+                }
+
+                if (pierwszy)
+                {
+                    ktoZacznie = losujKtoZacznie();
+                    ustawAktualnego(ktoZacznie);
+                }
+
+
+
 
 
                     while ((linia = in.readLine()) != null) {
                         if (linia.equalsIgnoreCase("/q")) {
                             break;
-                        } else {
+                        }
+                        if (linia.equalsIgnoreCase("/p")) {
+                            pokazPunkty();
+                        }
+                        else {
                             if (aktualnyUczestnik == this) {
+
                                 if ((linia.equalsIgnoreCase("/r"))) {
-                                    if ((uczestnicy.indexOf(this) + 1 == uczestnicy.size())) {
-                                        aktualnyUczestnik = uczestnicy.get(0);
-                                    } else {
-                                        aktualnyUczestnik = uczestnicy.get(uczestnicy.indexOf(this) + 1);
-                                    }
+                                    zakonczTure();
                                 } else {
-                                    wyslijDoWszystkich(linia);
+                                    graj();
                                 }
                             }
                         }
                     }
-
-
-
-                wyslijDoWszystkich("Opuścił grę");                              // jeżeli wpisał /q to opuszcza grę
-                System.out.println("Grę opuścił: " + nick);
-
+                    opuscGre();
             }
             else {
                 out.println("Serwer jest pełny! Komenda /q kończy połączenie.");   // to wysyłamy do klienta
@@ -139,17 +236,6 @@ class Uczestnik extends Thread {
                 socket.close(); // zamknięcie socketu
             } catch (IOException e) {
                 e.printStackTrace();
-            }finally {
-                synchronized(uczestnicy) {
-                    if ((uczestnicy.indexOf(this) + 1 == uczestnicy.size())) {
-                        aktualnyUczestnik = uczestnicy.get(0);
-                    } else {
-                        aktualnyUczestnik = uczestnicy.get(uczestnicy.indexOf(this) + 1);
-                    }
-                    uczestnicy.removeElement(this); // usunięcie bieżącego uczestnika z listy uczestników
-                    maxIloscGraczy--;
-
-                }
             }
         }
     }
